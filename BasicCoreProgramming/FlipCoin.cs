afe19ca9166d9db49bfd9293ce1fbaf858a07c11
Jebakani <vjishwarya@gmail.com>
2021-06-29 18:43:17 +0530

    [JEBAKANI] P1-Flip Coin Execution

---
+++ BasicCoreProgramming/FlipCoin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    class FlipCoin
+    {
+        public static void ReadInput()
+        {
+            Console.WriteLine("Flip coin program");
+            
+            //getting input from user
+            Console.WriteLine("Enter number of times the coin to be fliped :");
+            int numberOfTimeFlip = Convert.ToInt32(Console.ReadLine());
+
+            //creating object for random class
+            Random random = new Random();
+           
+            int head = 0;
+            int tail = 0;
+
+            //Calculating number of heads and tails for n times 
+            for(int trail=1;trail<=numberOfTimeFlip;trail++)
+            {
+                //getting the flip result
+                double headOrTail = random.NextDouble();
+
+                //selecting head or tail
+                if (headOrTail<0.5)
+                {
+                    tail++;
+                }
+                else
+                {
+                    head++;
+                }
+            }
+
+            Console.WriteLine("Number of head out of "+numberOfTimeFlip+" is : "+head);
+            Console.WriteLine("Number of tail out of "+numberOfTimeFlip+" is : "+tail);
+
+            //calling method that calculate the percentage
+            percentage(head, tail, numberOfTimeFlip);
+
+        }
+
+        // calculating the percentage of heads and tails
+        private static void percentage(int heads , int tails, int flips)
+        {
+            double percentageOfHead = (double)heads / flips * 100;
+            double percentageOfTail = (double)tails / flips * 100;
+            Console.WriteLine("Percentage of head "+percentageOfHead);
+            Console.WriteLine("Percentage of tail "+percentageOfTail);
+        }
+
+
+    }
+}
--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -4,9 +4,16 @@ namespace BasicCoreProgramming
 {
     class Program
     {
+        /// <summary>
+        /// P1- Executing the Flip coin program and finding the percentage
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic C# programs");
+
+            //calling static object for Flipcoin
+            FlipCoin.ReadInput();
             Console.Read();
             
         }
