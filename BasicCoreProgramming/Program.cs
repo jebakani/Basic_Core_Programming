65e2b08b7b11cfad934d3de47aa4f072f8fede03
Jebakani <vjishwarya@gmail.com>
2021-06-29 17:36:58 +0530

    [JEBAKANI] Computing Basic Core programming

---
+++ BasicCoreProgramming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicCoreProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic C# programs");
+            Console.Read();
+            
+        }
+    }
+}
