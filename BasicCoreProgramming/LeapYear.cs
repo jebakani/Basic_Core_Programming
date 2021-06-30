94562e1d443281d57c272d3f257ef8456c9a7933
Jebakani <vjishwarya@gmail.com>
2021-06-30 11:13:04 +0530

    [JEBAKANI] P2- Check for Leap year

---
+++ BasicCoreProgramming/LeapYear.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    class LeapYear
+    {
+        public static void ReadInput()
+        {
+            Console.WriteLine("leap year program");
+
+            //getting input from user
+            Console.WriteLine("Enter the year :");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            //calling the method that checks for leap year
+            CheckForLeapYear(year);
+
+        }
+
+        //method that check whether the given year is leap year or not
+        private static void CheckForLeapYear(int year)
+        {
+            //checks the condition for leap year
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} is the leap year",year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not the leap year",year);
+            }
+
+        }
+    }
+
+}
--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -6,21 +6,34 @@ namespace BasicCoreProgramming
     {
         /// <summary>
         /// P1- Executing the Flip coin program and finding the percentage
+        /// p2 - Executing leap year program
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic C# programs");
+
+            //creating menu to choose the program to be executed
             Console.WriteLine("Choose a program:");
             Console.WriteLine("1) Flip Coin");
+            Console.WriteLine("2) Leap Year");
             Console.WriteLine("0) To Exit");
+
+            //Read the choice from user
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
             {
+                //calling flip coin program
                 case 1:
                     FlipCoin.ReadInput();
                     break;
+
+                //calling leap year program
+                case 2:
+                    LeapYear.ReadInput();
+                    break;
+
                 default:
                     break;
             }
